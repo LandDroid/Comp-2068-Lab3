301489f1522cf368b429e58799371f1b41037dfc
LandDroid <landonlyle@live.ca>
2020-06-12 00:34:48 -0400

    Added files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Set our views directory
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jsx');
+app.set('view engine', 'ejs');
 
 //Our routes
 const routes = require('./routes.js');
--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -1,7 +1,13 @@
+const viewPath = ('pages');
+
 exports.home = (req, res) => {
-    res.send('Welcome home, from the controller');
+    res.render(`${viewPath}/home`, {
+        pageTitle: 'Welcome Home'
+    });
 };
 
 exports.about = (req, res) => {
-    res.send('About me, from the controller');
+    res.render(`${viewPath}/about`, {
+        pageTitle: 'About Me'
+    });
 };
